2fad5a0c1c3930f6cf80d05816df9c6f257deab8
sirflint07 <fajobimichael123@gmail.com>
2025-05-22 17:47:46 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Devlepoer's Portfolio",
+  description:
+    "Modern & Minimal Portfolio for Web Developers By Michael Olufemi",
+  icons: "/favicon.png",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+      </head>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ data/index.ts
@@ -0,0 +1,226 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title:
+      "I prioritize building dynamic web apps with MERN stack thaty are fuylly functional and responsive",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "My web dev stack of latest technologies",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Golden Designs Studio",
+    des: "A portfolio web project built using next JS for Golden Designs Studio.",
+    img: "/portfolio-1.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg"],
+    link: "https://golden-designs-studio.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Reminet - Landing Page",
+    des: "A simple single landing page built with Next JS where tech enthusiasts can learn web development and other profession.",
+    img: "/1.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg"],
+    link: "https://reminet.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "AI Image SaaS - Canva Application",
+    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "",
+  },
+  {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Michael was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Michael's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Michael is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Michael was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Michael's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Michael is the ideal partner.",
+    name: "Oliver Skipp",
+    title: "Photographer and Digital Editor, Sedex",
+  },
+  {
+    quote:
+      "Collaborating with Michael was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Michael's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Michael is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Michael was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Michael's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Michael is the ideal partner.",
+    name: "Edwin Benson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Michael was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Michael's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Michael is the ideal partner.",
+    name: "Sarajh Collins Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "MERN Developer",
+    desc: "Working as a freelance developer on freelnacing platforms like Fiverr to help potential clients to bring their vision to reality.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev - React Native",
+    desc: "Designeing and developing mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/sirflint07",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://x.com/golden_designs7",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/fajobi-michael-olufemi/",
+  },
+  {
+    id: 4,
+    img: "",
+    link: "https://www.youtube.com/channel/UC4L2PqObzbKbkk7VNiMsulw",
+  },
+];
